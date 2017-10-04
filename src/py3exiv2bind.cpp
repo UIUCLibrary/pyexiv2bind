c6ff0305e73701cfc2738ae5c6b38141f02b62ad
hborcher <hborcher@illinois.edu>
2017-10-04 15:16:41 -0500

    refactored exiv2 code to glue.cpp

--- src/py3exiv2bind.cpp
+++ src/py3exiv2bind.cpp
@@ -2,15 +2,9 @@
 // Created by hborcher on 9/15/2017.
 //
 
-#include "py3exiv2bind.h"
-#include <iostream>
 #include <pybind11/pybind11.h>
-#include <exiv2/exiv2.hpp>
+#include "glue.h"
 
-
-std::string exiv2_version() {
-    return Exiv2::version();
-}
 
 PYBIND11_MODULE(py3exiv2bind, m){
     m.doc() = "Glue code";
