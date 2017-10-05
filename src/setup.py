461f3de851d16f4f6248c3d4d56d6cf9b0af072c
hborcher <hborcher@illinois.edu>
2017-10-05 16:27:17 -0500

    add package data

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,7 @@ setup(
     name="py3exiv2bind",
     version="0.0.1",
     packages=['py3exiv2bind'],
+    package_data={'py3exiv2bind': ['*.pyd']},
     setup_requires=[
         "pytest-runner"
     ],
