b4df5b39e565ac79ba1a595e529f98795eaacb1d
hborcher <hborcher@illinois.edu>
2017-10-05 15:55:29 -0500

    added pytest-runner to setup_requires

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,9 @@ setup(
     name="py3exiv2bind",
     version="0.0.1",
     packages=['py3exiv2bind'],
+    setup_requires=[
+        "pytest-runner"
+    ],
     test_suite="tests",
     tests_require=['pytest'],
 
