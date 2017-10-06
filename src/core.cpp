eec4ebadd09e86c78dcccbf9aaa9d75bbc89ac58
hborcher <hborcher@illinois.edu>
2017-10-06 17:28:18 -0500

    PSR-45 exiv2 binding get_exif_metadata function

--- src/core.cpp
+++ src/core.cpp
@@ -3,10 +3,11 @@
 //
 
 #include <pybind11/pybind11.h>
-#include "glue.h"
+#include <glue.h>
 
 
 PYBIND11_MODULE(core, m){
     m.doc() = "Glue code";
     m.def("exiv2_version", &exiv2_version, "Just a exiv2_version check");
+    m.def("get_exif_metadata", &get_exif_metadata, "Get the exif metadata of a file");
 }
