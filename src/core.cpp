cf3e6715c98e8c96de27df7645d7cd559bc2b845
hborcher <hborcher@illinois.edu>
2017-10-06 17:48:39 -0500

    PSR-45 exiv2 binding: temp function doing nothing

--- src/core.cpp
+++ src/core.cpp
@@ -4,10 +4,12 @@
 
 #include <pybind11/pybind11.h>
 #include <glue.h>
-
+void exif_metadata(const std::string &filename){
+    auto metadata = get_exif_metadata(filename);
+}
 
 PYBIND11_MODULE(core, m){
     m.doc() = "Glue code";
     m.def("exiv2_version", &exiv2_version, "Just a exiv2_version check");
-    m.def("get_exif_metadata", &get_exif_metadata, "Get the exif metadata of a file");
+    m.def("get_exif_metadata", &exif_metadata, "Get the exif metadata of a file");
 }
