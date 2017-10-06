31bab141766e9fc8d0bab8ac32658e34934014b1
hborcher <hborcher@illinois.edu>
2017-10-06 17:50:00 -0500

    PSR-45 exiv2 binding added test_exif_metadata

--- tests/python_tests/test_run.py
+++ tests/python_tests/test_run.py
@@ -1,9 +1,21 @@
 # print("Running {}".format(__file__))
+import os
 import py3exiv2bind
+
 # exiv2_version = py3exiv2bind.exiv2_version()
 # print("Bound to exiv2, version {}".format(exiv2_version))
 # print("{} run succesfully".format(__file__))
 print("HEre")
+
+
 def test_exiv_version():
     exiv2_version = py3exiv2bind.exiv2_version()
     assert isinstance(exiv2_version, str)
+
+
+def test_exif_metadata():
+    sample_file = r"tests\sample_images\dummy.jp2"
+    assert os.path.exists(sample_file)
+    print("Found test file")
+    print("Testing {}".format(sample_file))
+    foo = py3exiv2bind.get_exif_metadata(sample_file)
