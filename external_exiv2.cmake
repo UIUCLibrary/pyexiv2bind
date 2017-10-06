24fc2762deaba87ebb4f0d2bf34303afde5edd43
hborcher <hborcher@illinois.edu>
2017-10-06 17:27:11 -0500

    PSR-45 exiv2 binding: refactored external deps logic into own files

--- external_exiv2.cmake
+++ external_exiv2.cmake
@@ -1,72 +1,55 @@
 include(ExternalProject)
-
-ExternalProject_Add(zlibsource
-        URL https://zlib.net/zlib-1.2.11.tar.gz
-        CMAKE_ARGS
-        -DINSTALL_BIN_DIR=""
-        -DINSTALL_LIB_DIR=${CMAKE_BINARY_DIR}/thirdparty/zlib/lib
-        -DINSTALL_INC_DIR=${CMAKE_BINARY_DIR}/thirdparty/zlib/include
-        -DINSTALL_MAN_DIR=""
-        -DINSTALL_PKGCONFIG_DIR=""
-
-        )
-ExternalProject_Get_Property(zlibsource install_dir)
+include(external_zlib.cmake)
+include(external_expat.cmake)
 message(STATUS "zlibsource install_dir = ${install_dir}")
-ExternalProject_Add(project_libexpat
-        URL https://github.com/libexpat/libexpat/archive/R_2_2_4.tar.gz
-        SOURCE_SUBDIR expat
-        CMAKE_ARGS
-        -DBUILD_examples=off
-        -DBUILD_shared=off
-        -DBUILD_tests=off
-        -DBUILD_tools=off
-        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-        #        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/thirdparty/expat
-        )
 
-
-ExternalProject_Get_Property(project_libexpat install_dir)
-add_library(libexpat STATIC IMPORTED )
-
-set_target_properties(libexpat PROPERTIES
-        INCLUDE_DIRECTORIES ${install_dir}/include
-        IMPORTED_LOCATION_DEBUG ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}expatd{CMAKE_STATIC_LIBRARY_SUFFIX}
-        IMPORTED_LOCATION_RELEASE ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}expat{CMAKE_STATIC_LIBRARY_SUFFIX})
-get_target_property(ff libexpat LOCATION)
-message(WARNING "IMPORTED_LOCATION = ${ff}")
-set(libexpat_install_dir ${install_dir})
-message(STATUS "libexpat_install_dir = ${libexpat_install_dir}")
 ExternalProject_Add(project_libexiv2
-        URL https://github.com/henryborchers/exiv2/archive/v0.26_deps.tar.gz
-        #        GIT_REPOSITORY https://github.com/henryborchers/exiv2.git
+#        URL https://github.com/henryborchers/exiv2/archive/v0.26_deps.tar.gz
+        URL https://github.com/henryborchers/exiv2/archive/v0.26.1_deps.tar.gz
+#        GIT_REPOSITORY https://github.com/Exiv2/exiv2.git
         CMAKE_ARGS
         -DEXIV2_ENABLE_DYNAMIC_RUNTIME=ON
         -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-        #        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/thirdparty/exiv2
         -DBUILD_SHARED_LIBS:BOOL=OFF
-        -DEXIV2_ENABLE_BUILD_SAMPLES:BOOL=OFF
+#        -DBUILD_SHARED_LIBS:BOOL=OFF
+        -DEXIV2_BUILD_SAMPLES:BOOL=OFF
         -DEXIV2_ENABLE_TOOLS:BOOL=ON
         -DEXIV2_ENABLE_CURL:BOOL=ON
         -DEXIV2_ENABLE_PNG:BOOL=OFF
         -DEXIV2_ENABLE_XMP:BOOL=OFF
-        -DEXPAT_INCLUDE_DIR=${libexpat_install_dir}/include
-        -DEXPAT_LIBRARY=${libexpat_install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}expat$<$<CONFIG:Debug>:d>${CMAKE_STATIC_LIBRARY_SUFFIX}
-        -DZLIB_INCLUDE_DIR=${CMAKE_BINARY_DIR}/thirdparty/zlib/include
-        -DZLIB_LIBRARY=${CMAKE_BINARY_DIR}/thirdparty/zlib/lib/${CMAKE_STATIC_LIBRARY_PREFIX}zlibstatic${CMAKE_STATIC_LIBRARY_SUFFIX}
+        -DEXPAT_INCLUDE_DIR=$<TARGET_PROPERTY:libexpat,INCLUDE_DIRECTORIES>
+        -DEXPAT_LIBRARY=$<TARGET_FILE:libexpat>
+        -DZLIB_INCLUDE_DIR=$<TARGET_PROPERTY:zlib,INCLUDE_DIRECTORIES>
+        -DZLIB_LIBRARY=$<TARGET_FILE:zlib>
         -DICONV_INCLUDE_DIR=""
         -DICONV_LIBRARY=""
+        DEPENDS libexpat zlib
         )
 add_dependencies(project_libexiv2 zlibsource project_libexpat)
 ExternalProject_Get_Property(project_libexiv2 install_dir)
 message(STATUS "project_libexiv2 install_dir = ${install_dir}")
 add_library(libexiv2 STATIC IMPORTED)
+
 add_dependencies(libexiv2 project_libexiv2)
-
-
 set_property(TARGET libexiv2 PROPERTY INCLUDE_DIRECTORIES ${install_dir}/include)
-
 set_target_properties(libexiv2 PROPERTIES
         IMPORTED_LOCATION_DEBUG ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}exiv2d${CMAKE_STATIC_LIBRARY_SUFFIX}
         IMPORTED_LOCATION_RELEASE ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}exiv2${CMAKE_STATIC_LIBRARY_SUFFIX}
         CXX_STANDARD 98
+        )
+
+add_library(libxmp STATIC IMPORTED)
+add_dependencies(libxmp project_libexiv2)
+
+set_target_properties(libxmp PROPERTIES
+        IMPORTED_LOCATION_DEBUG ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}xmpd${CMAKE_STATIC_LIBRARY_SUFFIX}
+        IMPORTED_LOCATION_RELEASE ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}xmp${CMAKE_STATIC_LIBRARY_SUFFIX}
+#        CXX_STANDARD 98
+        )
+
+add_custom_target(dummy_xmp)
+add_custom_command(TARGET dummy_xmp
+        COMMENT "adfasdfasdddddddddddddf $<TARGET_FILE:libxmp>"
+        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_FILE:libxmp>
+        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_PROPERTY:libxmp,INCLUDE_DIRECTORIES>
         )
--- external_expat.cmake
+++ external_expat.cmake
@@ -5,9 +5,11 @@ ExternalProject_Add(project_libexpat
         CMAKE_ARGS
         -DBUILD_examples=off
         -DBUILD_shared=off
-        -DBUILD_tests=off
+        -DBUILD_tests=ON
         -DBUILD_tools=off
         -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+        TEST_BEFORE_INSTALL 1
+#        TEST_COMMAND=runtests${CMAKE_EXECUTABLE_SUFFIX}
         #        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/thirdparty/expat
         )
 
@@ -24,8 +26,9 @@ add_dependencies(libexpat project_libexpat)
 #add_custom_target(dummy
 #        COMMENT ""
 #        )
-#add_custom_command(TARGET dummy
-#        COMMENT "adfasdfasdddddddddddddf $<TARGET_FILE:libexpat>"
-#        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_FILE:libexpat>
-#        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_PROPERTY:libexpat,INCLUDE_DIRECTORIES>
-#        )
+add_custom_target(dummy_expat)
+add_custom_command(TARGET dummy_expat
+        COMMENT "adfasdfasdddddddddddddf $<TARGET_FILE:libexpat>"
+        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_FILE:libexpat>
+        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_PROPERTY:libexpat,INCLUDE_DIRECTORIES>
+        )
