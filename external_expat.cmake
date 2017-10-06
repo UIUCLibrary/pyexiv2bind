a728c5a5b7f276b98e8d6c64c247d17c1c70a398
hborcher <hborcher@illinois.edu>
2017-10-06 15:32:23 -0500

    init commit

---
+++ external_expat.cmake
@@ -0,0 +1,31 @@
+include(ExternalProject)
+ExternalProject_Add(project_libexpat
+        URL https://github.com/libexpat/libexpat/archive/R_2_2_4.tar.gz
+        SOURCE_SUBDIR expat
+        CMAKE_ARGS
+        -DBUILD_examples=off
+        -DBUILD_shared=off
+        -DBUILD_tests=off
+        -DBUILD_tools=off
+        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+        #        -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/thirdparty/expat
+        )
+
+
+ExternalProject_Get_Property(project_libexpat install_dir)
+add_library(libexpat STATIC IMPORTED )
+
+set_target_properties(libexpat PROPERTIES
+        INCLUDE_DIRECTORIES ${install_dir}/include
+        IMPORTED_LOCATION_DEBUG ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}expatd${CMAKE_STATIC_LIBRARY_SUFFIX}
+        IMPORTED_LOCATION_RELEASE ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}expat${CMAKE_STATIC_LIBRARY_SUFFIX})
+#get_target_property(ff libexpat LOCATION)
+add_dependencies(libexpat project_libexpat)
+#add_custom_target(dummy
+#        COMMENT ""
+#        )
+#add_custom_command(TARGET dummy
+#        COMMENT "adfasdfasdddddddddddddf $<TARGET_FILE:libexpat>"
+#        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_FILE:libexpat>
+#        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_PROPERTY:libexpat,INCLUDE_DIRECTORIES>
+#        )
---
+++ external_zlib.cmake
@@ -0,0 +1,29 @@
+include(ExternalProject)
+ExternalProject_Add(zlibsource
+        URL https://zlib.net/zlib-1.2.11.tar.gz
+        CMAKE_ARGS
+        -DINSTALL_BIN_DIR=""
+        -DINSTALL_LIB_DIR=<INSTALL_DIR>/lib
+        -DINSTALL_INC_DIR=<INSTALL_DIR>/include
+        -DINSTALL_MAN_DIR=""
+        -DINSTALL_PKGCONFIG_DIR=""
+
+        )
+ExternalProject_Get_Property(zlibsource install_dir)
+
+add_library(zlib STATIC IMPORTED )
+add_dependencies(zlib zlibsource)
+set_target_properties(zlib PROPERTIES
+        INCLUDE_DIRECTORIES ${install_dir}/include
+        IMPORTED_LOCATION_DEBUG ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}zlibstaticd${CMAKE_STATIC_LIBRARY_SUFFIX}
+        IMPORTED_LOCATION_RELEASE ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}zlibstatic{CMAKE_STATIC_LIBRARY_SUFFIX}
+        )
+#add_custom_target(dummy
+#        COMMENT ""
+#        DEPENDS zlib
+#        )
+#add_custom_command(TARGET dummy
+#        COMMENT "adfasdfasdddddddddddddf $<TARGET_FILE:zlib>"
+#        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_FILE:zlib>
+#        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_PROPERTY:zlib,INCLUDE_DIRECTORIES>
+#        )
