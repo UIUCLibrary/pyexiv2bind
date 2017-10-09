7d56e5e18373e76465ca050cd61ccf58ccbdf948
hborcher <hborcher@illinois.edu>
2017-10-09 13:51:29 -0500

    PSR-45 exiv2 binding, get_exif_metadata now returns a dictionary

--- src/core.cpp
+++ src/core.cpp
@@ -3,13 +3,17 @@
 //
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <glue.h>
-void exif_metadata(const std::string &filename){
-    auto metadata = get_exif_metadata(filename);
+#include <map>
+
+std::map<std::string, std::string> exif_metadata(const std::string &filename) {
+    std::map<std::string, std::string> metadata;
+    return get_exif_metadata2(filename);
 }
 
-PYBIND11_MODULE(core, m){
+PYBIND11_MODULE(core, m) {
     m.doc() = "Glue code";
     m.def("exiv2_version", &exiv2_version, "Just a exiv2_version check");
-    m.def("get_exif_metadata", &exif_metadata, "Get the exif metadata of a file");
+    m.def("get_exif_metadata", &exif_metadata, "Get the exif metadata of a file", pybind11::arg("filename"));
 }
