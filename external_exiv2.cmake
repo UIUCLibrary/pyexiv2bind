1740bd054b3e8e2b65a25de40c8c53f843bf14e9
hborcher <hborcher@illinois.edu>
2017-10-09 13:51:44 -0500

    PSR-45 exiv2 binding, formatting

--- external_exiv2.cmake
+++ external_exiv2.cmake
@@ -8,22 +8,24 @@ ExternalProject_Add(project_libexiv2
         URL https://github.com/henryborchers/exiv2/archive/v0.26.1_deps.tar.gz
 #        GIT_REPOSITORY https://github.com/Exiv2/exiv2.git
         CMAKE_ARGS
-        -DEXIV2_ENABLE_DYNAMIC_RUNTIME=ON
-        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-        -DBUILD_SHARED_LIBS:BOOL=OFF
-#        -DBUILD_SHARED_LIBS:BOOL=OFF
-        -DEXIV2_BUILD_SAMPLES:BOOL=OFF
-        -DEXIV2_ENABLE_TOOLS:BOOL=ON
-        -DEXIV2_ENABLE_CURL:BOOL=ON
-        -DEXIV2_ENABLE_PNG:BOOL=OFF
-        -DEXIV2_ENABLE_XMP:BOOL=OFF
-        -DEXPAT_INCLUDE_DIR=$<TARGET_PROPERTY:libexpat,INCLUDE_DIRECTORIES>
-        -DEXPAT_LIBRARY=$<TARGET_FILE:libexpat>
-        -DZLIB_INCLUDE_DIR=$<TARGET_PROPERTY:zlib,INCLUDE_DIRECTORIES>
-        -DZLIB_LIBRARY=$<TARGET_FILE:zlib>
-        -DICONV_INCLUDE_DIR=""
-        -DICONV_LIBRARY=""
-        DEPENDS libexpat zlib
+            -DEXIV2_ENABLE_DYNAMIC_RUNTIME=ON
+            -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+            -DBUILD_SHARED_LIBS:BOOL=OFF
+            -DEXIV2_BUILD_SAMPLES:BOOL=OFF
+            -DEXIV2_ENABLE_TOOLS:BOOL=ON
+            -DEXIV2_ENABLE_CURL:BOOL=ON
+            -DEXIV2_ENABLE_PNG:BOOL=OFF
+            -DEXIV2_ENABLE_XMP:BOOL=OFF
+            -DEXPAT_INCLUDE_DIR=$<TARGET_PROPERTY:libexpat,INCLUDE_DIRECTORIES>
+            -DEXPAT_LIBRARY=$<TARGET_FILE:libexpat>
+            -DZLIB_INCLUDE_DIR=$<TARGET_PROPERTY:zlib,INCLUDE_DIRECTORIES>
+            -DZLIB_LIBRARY=$<TARGET_FILE:zlib>
+            -DICONV_INCLUDE_DIR=""
+            -DICONV_LIBRARY=""
+        DEPENDS
+            libexpat
+            zlib
+
         )
 add_dependencies(project_libexiv2 zlibsource project_libexpat)
 ExternalProject_Get_Property(project_libexiv2 install_dir)
