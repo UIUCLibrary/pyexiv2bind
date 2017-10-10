5dcf9c5c95d0e6efb6fec583d188a9836c1d815e
hborcher <hborcher@illinois.edu>
2017-10-10 11:49:42 -0500

    formatting

--- build_python_module.cmake
+++ build_python_module.cmake
@@ -5,9 +5,9 @@ add_custom_target(py3exiv2bind
         COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_SOURCE_DIR}/setup.cfg $<TARGET_FILE_DIR:core>/..
         COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_SOURCE_DIR}/tox.ini $<TARGET_FILE_DIR:core>/..
         BYPRODUCTS
-        setup.py
-        setup.cfg
-        tox.ini
+            setup.py
+            setup.cfg
+            tox.ini
         )
 foreach (_file ${python_files})
     add_custom_command(TARGET py3exiv2bind
