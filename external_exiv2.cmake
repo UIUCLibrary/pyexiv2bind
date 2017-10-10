653da942e70b25afb8d7e674974c0518aa73dd31
hborcher <hborcher@illinois.edu>
2017-10-10 15:25:20 -0500

    renamed to exiv2lib because that's what findExiv2 uses

--- external_exiv2.cmake
+++ external_exiv2.cmake
@@ -22,6 +22,7 @@ ExternalProject_Add(project_libexiv2
         URL https://github.com/henryborchers/exiv2/archive/v0.26.1_deps.tar.gz
         #        GIT_REPOSITORY https://github.com/Exiv2/exiv2.git
         CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE=$<CONFIG>
         -DEXIV2_BUILD_EXIV2_COMMAND:BOOL=ON
         -DEXIV2_ENABLE_DYNAMIC_RUNTIME=ON
         -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
@@ -46,28 +47,15 @@ ExternalProject_Add(project_libexiv2
 #add_dependencies(project_libexiv2 zlibsource project_libexpat)
 ExternalProject_Get_Property(project_libexiv2 install_dir)
 message(STATUS "project_libexiv2 install_dir = ${install_dir}")
-add_library(libexiv2 STATIC IMPORTED)
+add_library(exiv2lib STATIC IMPORTED)
 
-add_dependencies(libexiv2 project_libexiv2)
-set_property(TARGET libexiv2 PROPERTY INCLUDE_DIRECTORIES ${install_dir}/include)
-set_target_properties(libexiv2 PROPERTIES
+
+#set_property(TARGET libexiv2 )
+
+set_target_properties(exiv2lib PROPERTIES
+        INCLUDE_DIRECTORIES ${install_dir}/include
         IMPORTED_LOCATION_DEBUG ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}exiv2d${CMAKE_STATIC_LIBRARY_SUFFIX}
         IMPORTED_LOCATION_RELEASE ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}exiv2${CMAKE_STATIC_LIBRARY_SUFFIX}
         CXX_STANDARD 98
         )
-
-#add_library(libxmp STATIC IMPORTED)
-#add_dependencies(libxmp project_libexiv2)
-#
-#set_target_properties(libxmp PROPERTIES
-#        IMPORTED_LOCATION_DEBUG ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}xmpd${CMAKE_STATIC_LIBRARY_SUFFIX}
-#        IMPORTED_LOCATION_RELEASE ${install_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}xmp${CMAKE_STATIC_LIBRARY_SUFFIX}
-#        #        CXX_STANDARD 98
-#        )
-#
-#add_custom_target(dummy_xmp)
-#add_custom_command(TARGET dummy_xmp
-#        COMMENT "adfasdfasdddddddddddddf $<TARGET_FILE:libxmp>"
-#        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_FILE:libxmp>
-#        COMMAND ${CMAKE_COMMAND} -E echo $<TARGET_PROPERTY:libxmp,INCLUDE_DIRECTORIES>
-#        )
+add_dependencies(exiv2lib project_libexiv2)
