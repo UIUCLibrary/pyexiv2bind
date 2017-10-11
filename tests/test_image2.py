ea61de4a2af12abecba800bc0cb05d531ed20ce4
hborcher <hborcher@illinois.edu>
2017-10-11 15:46:31 -0500

    added test for iptc

--- tests/test_image2.py
+++ tests/test_image2.py
@@ -30,3 +30,16 @@ def test_image2_exif():
     assert isinstance(exivf_metadata, dict)
     print(exivf_metadata)
     assert exivf_metadata['Exif.Image.Artist'] == "University of Illinois Library"
+
+
+
+def test_image2_iptc():
+
+    sample_file = os.path.join(os.path.dirname(__file__), "sample_images/dummy.jp2")
+    my_image = Image2(sample_file)
+    iptc_metadata = my_image.iptc
+    assert isinstance(iptc_metadata, dict)
+    print(iptc_metadata)
+    assert iptc_metadata['Iptc.Application2.ObjectName'] == "Mapping History - University Archives"
+
+
