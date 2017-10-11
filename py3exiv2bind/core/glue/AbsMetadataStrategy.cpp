76fc97ac9e4e1e2f65e192a5680fa6d389e077f3
hborcher <hborcher@illinois.edu>
2017-10-11 17:04:40 -0500

    init commit

---
+++ py3exiv2bind/core/glue/AbsMetadataStrategy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hborcher on 10/11/2017.
+//
+
+#include "AbsMetadataStrategy.h"
---
+++ py3exiv2bind/core/glue/MetadataProcessor.h
@@ -0,0 +1,26 @@
+//
+// Created by hborcher on 10/11/2017.
+//
+
+#ifndef PYEXIV2BIND_METADATA_PROCESSOR_H
+#define PYEXIV2BIND_METADATA_PROCESSOR_H
+
+
+#include <string>
+#include <map>
+#include <memory>
+#include "AbsMetadataStrategy.h"
+#include "MetadataStrategies.h"
+
+struct MetadataProcessor {
+private:
+    std::unique_ptr<AbsMetadataStrategy> metadata_strategy;
+    std::map<std::string, std::string> metadata;
+public:
+    void build(const Exiv2::Image::AutoPtr &image);
+    const std::map<std::string, std::string> &getMetadata() const;
+    void set_output_format(MetadataStrategies);
+};
+
+
+#endif //PYEXIV2BIND_METADATA_PROCESSOR_H
