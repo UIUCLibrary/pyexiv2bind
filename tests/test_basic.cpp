3a0a819f756e83d8fddc04a515e7a3b565096b77
hborcher <hborcher@illinois.edu>
2017-10-12 16:30:26 -0500

    removed tests to removed code

--- tests/test_basic.cpp
+++ tests/test_basic.cpp
@@ -3,25 +3,11 @@
 //
 #include <catch.hpp>
 #include <glue/glue.h>
-#include <glue/Image2.h>
+#include <glue/Image.h>
 
-
-TEST_CASE("get_exif_metadata", "[glue]") {
+TEST_CASE("Try Image", "[glue]"){
     const std::string filename = "tests/sample_images/dummy.jp2";
-    auto metadata = get_exif_metadata(filename);
-    REQUIRE(!metadata.empty());
-}
-
-TEST_CASE("get_exif_metadata2", "[glue]") {
-    const std::string filename = "tests/sample_images/dummy.jp2";
-    auto metadata = get_exif_metadata2(filename);
-
-    REQUIRE(!metadata.empty());
-}
-
-TEST_CASE("Try Image2", "[glue]"){
-    const std::string filename = "tests/sample_images/dummy.jp2";
-    Image2 i(filename);
+    Image i(filename);
     SECTION("File is correctly added"){
         REQUIRE(i.filename == filename);
     }
