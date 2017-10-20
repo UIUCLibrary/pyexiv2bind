611a785fff3ff5d9cd58f42bd65eeb2031e148fb
hborcher <hborcher@illinois.edu>
2017-10-20 13:57:36 -0500

    if env var EXIV2_DIR is set to the CMake module path of an exiv2 build, it will use that version to compile

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+import os
 from skbuild import setup
-
+cmake_args = []
+if "EXIV2_DIR" in os.environ:
+    if os.path.exists(os.environ['EXIV2_DIR']):
+        print("Detected that the environment variable EXIV2_DIR is set to {}.".format(os.environ['EXIV2_DIR']))
+        cmake_args.append('-DEXIV2_DIR:PATH={}'.format(os.environ['EXIV2_DIR']))
 setup(
     name="py3exiv2bind",
-    version="0.0.1",
+    version="0.0.2",
     packages=['py3exiv2bind'],
     # package_data={'py3exiv2bind': ['*.pyd', '*.so']},
     setup_requires=[
         "pytest-runner"
     ],
+    cmake_args=cmake_args,
     test_suite="tests",
     tests_require=['pytest'],
     zip_safe=False,
