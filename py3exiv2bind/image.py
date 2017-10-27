88dc4b0e5ca0556dc6b244ebd17306607a36e7da
Henry Borchers <henryborchers@yahoo.com>
2017-10-27 17:10:21 -0500

    add icc support

--- py3exiv2bind/image.py
+++ py3exiv2bind/image.py
@@ -1,6 +1,7 @@
 import os
 
 from . import core
+from . import icc
 import collections
 
 class Image(core.Image):
@@ -9,3 +10,8 @@ class Image(core.Image):
             raise FileNotFoundError("Unable to locate {}.".format(args[0]))
         super().__init__(*args, **kwargs)
         self.metadata = collections.ChainMap(self.exif, self.iptc, self.xmp)
+
+    def icc(self):
+        unpacked =icc.unpack_binary(self.get_icc_profile_data())
+
+        return unpacked._asdict()
