b47b1a8a22c70689045064f15ae0677e772d12af
Henry Borchers <henryborchers@yahoo.com>
2017-10-27 11:44:58 -0500

    check for invalid file before it's pushed to C++

--- py3exiv2bind/image.py
+++ py3exiv2bind/image.py
@@ -1,7 +1,11 @@
+import os
+
 from . import core
 import collections
 
 class Image(core.Image):
     def __init__(self, *args, **kwargs):
+        if not os.path.exists(args[0]):
+            raise FileNotFoundError("Unable to locate {}.".format(args[0]))
         super().__init__(*args, **kwargs)
         self.metadata = collections.ChainMap(self.exif, self.iptc, self.xmp)
