1980495c5e060c2277bf6d02c3a555716b0e2ae3
hborcher <hborcher@illinois.edu>
2017-10-30 10:44:52 -0500

    added docstring

--- py3exiv2bind/image.py
+++ py3exiv2bind/image.py
@@ -4,9 +4,17 @@ from . import core
 from . import icc
 import collections
 
+
 class Image(core.Image):
     INVALID_FORMATS_FOR_ICC = [".jp2"]
+
     def __init__(self, *args, **kwargs):
+        """
+        Loads the file to get information about it.
+        Args:
+            *args:
+            **kwargs:
+        """
         if not os.path.exists(args[0]):
             raise FileNotFoundError("Unable to locate {}.".format(args[0]))
         super().__init__(*args, **kwargs)
@@ -16,6 +24,6 @@ class Image(core.Image):
         extension = os.path.splitext(self.filename)[1].lower()
         if extension in Image.INVALID_FORMATS_FOR_ICC:
             raise AttributeError("{} files not currently supported for ICC data".format(extension))
-        unpacked =icc.unpack_binary(self.get_icc_profile_data())
+        unpacked = icc.unpack_binary(self.get_icc_profile_data())
 
         return unpacked._asdict()
