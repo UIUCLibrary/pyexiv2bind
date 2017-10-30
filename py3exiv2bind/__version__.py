ac923ddd276d5336ade0d18f1c956a12ed8fc234
Henry Borchers <henryborchers@yahoo.com>
2017-10-30 12:29:09 -0500

    init commit

---
+++ py3exiv2bind/__version__.py
@@ -0,0 +1,7 @@
+__title__ = 'py3exiv2bind'
+__version__ = '0.0.3b1'
+__maintainer__ = "Henry Borchers"
+__maintainer_email__ = "hborcher@illinois.edu",
+__url__ = 'https://github.com/UIUCLibrary/pyexiv2bind'
+__author__ = 'University of Illinois at Urbana Champaign'
+__description__ = "Exiv2 bindings for Python3"
