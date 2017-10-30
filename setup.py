3d5b10d4a0939fa9ac8d5ae004869f3cbb3441a2
Henry Borchers <henryborchers@yahoo.com>
2017-10-30 12:27:48 -0500

    get metadata from __version__.py file

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
 import os
 from skbuild import setup
+
+
+def get_metadata():
+    about = {}
+
+    metadata_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'py3exiv2bind', '__version__.py')
+
+    with open(metadata_file, 'r', encoding='utf-8') as f:
+        exec(f.read(), about)
+
+    return about
+
+
+metadata = get_metadata()
+
+with open('README.rst', 'r', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
 cmake_args = []
 if "EXIV2_DIR" in os.environ:
     if os.path.exists(os.environ['EXIV2_DIR']):
         print("Detected that the environment variable EXIV2_DIR is set to {}.".format(os.environ['EXIV2_DIR']))
         cmake_args.append('-DEXIV2_DIR:PATH={}'.format(os.environ['EXIV2_DIR']))
 setup(
-    name="py3exiv2bind",
-    version="0.0.3b1",
+    name=metadata["__title__"],
+    version=metadata["__version__"],
     # author="University of Illinois at Urbana Champaign",
-    maintainer="Henry Borchers",
-    maintainer_email="hborcher@illinois.edu",
+    maintainer=metadata["__maintainer__"],
+    maintainer_email=metadata["__maintainer_email__"],
     packages=['py3exiv2bind'],
     setup_requires=[
         "pytest-runner"
@@ -19,6 +37,7 @@ setup(
     test_suite="tests",
     tests_require=['pytest'],
     zip_safe=False,
-    url="https://github.com/UIUCLibrary/pyexiv2bind",
+    url=metadata["__url__"],
+    descriptions=metadata["__description__"]
 
 )
