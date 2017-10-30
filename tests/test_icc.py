b1db0c0f1b206291ffb864ccde0ac207ac3b4a11
hborcher <hborcher@illinois.edu>
2017-10-30 10:40:37 -0500

    Added test to make sure that nobody actually tries to access icc data for a jp2 file until the feature has been added on the exiv2 side

--- tests/test_icc.py
+++ tests/test_icc.py
@@ -1,6 +1,6 @@
 import os
 from pprint import pprint
-
+import pytest
 from py3exiv2bind import Image
 
 def test_icc_file():
@@ -8,3 +8,9 @@ def test_icc_file():
     my_image = Image(sample_file)
     icc = my_image.icc()
     pprint(icc)
+
+def test_icc_on_jp2_file():
+    sample_file = os.path.join(os.path.dirname(__file__), "sample_images/dummy.jp2")
+    my_image = Image(sample_file)
+    with pytest.raises(AttributeError):
+        icc = my_image.icc()
