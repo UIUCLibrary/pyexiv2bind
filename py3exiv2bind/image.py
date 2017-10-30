668d2ac1aa89a4fa196b72a5555bb6d4fb3cc76e
hborcher <hborcher@illinois.edu>
2017-10-30 10:41:18 -0500

    Make sure that nobody tries to access icc data for a jp2 file until the feature has been added on the exiv2 side

--- py3exiv2bind/image.py
+++ py3exiv2bind/image.py
@@ -5,6 +5,7 @@ from . import icc
 import collections
 
 class Image(core.Image):
+    INVALID_FORMATS_FOR_ICC = [".jp2"]
     def __init__(self, *args, **kwargs):
         if not os.path.exists(args[0]):
             raise FileNotFoundError("Unable to locate {}.".format(args[0]))
@@ -12,6 +13,9 @@ class Image(core.Image):
         self.metadata = collections.ChainMap(self.exif, self.iptc, self.xmp)
 
     def icc(self):
+        extension = os.path.splitext(self.filename)[1].lower()
+        if extension in Image.INVALID_FORMATS_FOR_ICC:
+            raise AttributeError("{} files not currently supported for ICC data".format(extension))
         unpacked =icc.unpack_binary(self.get_icc_profile_data())
 
         return unpacked._asdict()
