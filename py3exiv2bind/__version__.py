158b2eb7f1069d3613189e972256f8f4ff36171a
hborcher <hborcher@illinois.edu>
2017-10-30 17:33:39 -0500

    out of beta

--- py3exiv2bind/__version__.py
+++ py3exiv2bind/__version__.py
@@ -1,5 +1,5 @@
 __title__ = 'py3exiv2bind'
-__version__ = '0.0.3b1'
+__version__ = '0.0.3'
 __maintainer__ = "Henry Borchers"
 __maintainer_email__ = "hborcher@illinois.edu",
 __url__ = 'https://github.com/UIUCLibrary/pyexiv2bind'
