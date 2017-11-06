d4435b0ea787c127103d1f5a8e426902220e66ef
Henry Borchers <henryborchers@yahoo.com>
2017-11-06 13:00:08 -0600

    use make_dictionary template

--- py3exiv2bind/core/glue/ExifStrategy.cpp
+++ py3exiv2bind/core/glue/ExifStrategy.cpp
@@ -3,27 +3,14 @@
 //
 
 #include "ExifStrategy.h"
-
+#include "make_dictionary.h"
 std::map<std::string, std::string> ExifStrategy::load(const Exiv2::Image::AutoPtr &image) {
-    std::map<std::string, std::string> metadata;
     try{
-        Exiv2::ExifData &exifData = image->exifData();
-
-        if(exifData.empty()){
-
-            return std::map<std::string, std::string>();
-        }
-
-        auto end = exifData.end();
-        for (auto md = exifData.begin(); md != end; md++){
-            metadata[md->key()] = md->value().toString();
-        }
-
+        return make_dictionary(image->exifData());
     }catch (Exiv2::AnyError &e){
 //        TODO: Handle errors
         std::cerr << e.what() <<std::endl;
         throw;
     }
 //    TODO: return the metadata a vector
-    return metadata;
 }
