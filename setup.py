d217859f11f10e9e28dbd7a4f347e1817f26630b
hborcher <hborcher@illinois.edu>
2017-11-14 16:38:38 -0600

    moved the packages back to setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from skbuild import setup
 
 
 cmake_args = ["-Dpyexiv2bind_experimental_jp2_support=ON"]
+
 if "EXIV2_DIR" in os.environ:
     if os.path.exists(os.environ['EXIV2_DIR']):
         print("Detected that the environment variable EXIV2_DIR is set to {}.".format(os.environ['EXIV2_DIR']))
         cmake_args.append('-DEXIV2_DIR:PATH={}'.format(os.environ['EXIV2_DIR']))
+
 setup(
     setup_requires=[
         "pytest-runner"
     ],
+    packages=['py3exiv2bind'],
     cmake_args=cmake_args,
     test_suite="tests",
     tests_require=['pytest'],
