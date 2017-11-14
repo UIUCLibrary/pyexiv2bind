74c2a0ce5ceaf426e8e24d4e80f3937379bbf997
hborcher <hborcher@illinois.edu>
2017-11-14 12:25:04 -0600

    moved version metadata into setup.cfg

--- py3exiv2bind/__version__.py
+++
@@ -1,7 +0,0 @@
-__title__ = 'py3exiv2bind'
-__version__ = '0.0.3'
-__maintainer__ = "Henry Borchers"
-__maintainer_email__ = "hborcher@illinois.edu",
-__url__ = 'https://github.com/UIUCLibrary/pyexiv2bind'
-__author__ = 'University of Illinois at Urbana Champaign'
-__description__ = "Exiv2 bindings for Python3"
