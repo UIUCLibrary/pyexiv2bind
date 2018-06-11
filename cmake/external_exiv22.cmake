20b1e0ddd40fa9b8c58c88225073fa19b230c032
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-06-11 16:45:59 -0500

    exiv2 version is now a cache variable

--- cmake/external_exiv22.cmake
+++ cmake/external_exiv22.cmake
@@ -1,6 +1,8 @@
 include(FetchContent)
 include(ExternalProject)
 
+set(EXIV2_VERSION_TAG 0d6abb5b5130fbce8bdc398b6728d225838bb382 CACHE STRING "Git tag of version of exiv2 to build")
+
 ###########################################################################################
 # Google ZLIB
 ###########################################################################################
@@ -14,9 +16,6 @@ FetchContent_GetProperties(zlib)
 if (NOT zlib_POPULATED)
     FetchContent_Populate(zlib)
     add_subdirectory(${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
-#        set_target_properties(zlib PROPERTIES EXCLUE_FROM_ALL YES)
-#    get_target_property(is_all zlib TYPE)
-#    message(WARNING "is_all =${is_all}")
 endif ()
 
 
@@ -39,32 +38,10 @@ if (NOT expat_POPULATED)
     add_subdirectory(${expat_SOURCE_DIR}/expat ${expat_BINARY_DIR})
 endif ()
 
-
-###########################################################################################
-# Google test build
-###########################################################################################
-#
-#FetchContent_Declare(
-#        googletest
-#        GIT_REPOSITORY https://github.com/google/googletest.git
-#        GIT_TAG release-1.8.0
-#)
-#FetchContent_GetProperties(googletest)
-#if (NOT googletest_POPULATED)
-#    FetchContent_Populate(googletest)
-#    option(BUILD_GTEST "" ON)
-#    option(BUILD_GMOCK "" FALSE)
-#    option(BUILD_SHARED_LIBS "" ON)
-#    option(gtest_disable_pthreads "" TRUE)
-#    option(gtest_force_shared_crt "" TRUE)
-#    add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
-#endif ()
-
 FetchContent_Declare(
         libexiv2
         GIT_REPOSITORY https://github.com/Exiv2/exiv2.git
-        GIT_TAG 0d6abb5b5130fbce8bdc398b6728d225838bb382
-#        GIT_REPOSITORY https://github.com/UIUCLibrary/exiv2.git
+        GIT_TAG ${EXIV2_VERSION_TAG}
         PATCH_COMMAND
             COMMAND git apply ${PROJECT_SOURCE_DIR}/patches/Make_Subproject_possible.patch
 )
@@ -72,7 +49,7 @@ FetchContent_GetProperties(libexiv2)
 if (NOT libexiv2_POPULATED)
     FetchContent_Populate(libexiv2)
     option(BUILD_SHARED_LIBS "" OFF)
-#    set(BUILD_SHARED_LIBS OFF)
+    set(BUILD_SHARED_LIBS OFF CACHE BOOL "")
     option(EXIV2_BUILD_UNIT_TESTS "" OFF)
     option(EXIV2_ENABLE_DYNAMIC_RUNTIME "" ON)
     set(EXPAT_LIBRARY $<TARGET_FILE:expat>)
@@ -81,15 +58,9 @@ if (NOT libexiv2_POPULATED)
     set(ZLIB_LIBRARY $<TARGET_FILE:zlibstatic>)
     set(EXIV2_BUILD_SAMPLES OFF)
     option(EXIV2_BUILD_SAMPLES "" OFF)
-#    set(GTEST_INCLUDE_DIR $<TARGET_PROPERTY:gtest,INCLUDE_DIRECTORIES>)
-#    set(GTEST_LIBRARY $<TARGET_LINKER_FILE:gtest>)
-#    set(GTEST_LIBRARY_DEBUG $<TARGET_LINKER_FILE:gtest>)
-#    set(GTEST_MAIN_LIBRARY $<TARGET_LINKER_FILE:gtest_main>)
-#    set(GTEST_MAIN_LIBRARY_DEBUG $<TARGET_LINKER_FILE:gtest_main>)
     add_subdirectory(${libexiv2_SOURCE_DIR} ${libexiv2_BINARY_DIR})
     include_directories(${libexiv2_BINARY_DIR})
     add_dependencies(exiv2lib zlibstatic)
-#    add_dependencies(unit_tests gtest gtest_main)
 endif ()
 add_dependencies(xmp expat)
 
