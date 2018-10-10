c3424ad35fcc42b962c7550c36862241da67596b
Henry Borchers <henryborchers@yahoo.com>
2018-10-10 12:52:23 -0500

    pep8

--- tests/test_image.py
+++ tests/test_image.py
@@ -2,10 +2,12 @@ import os
 import pytest
 from py3exiv2bind import Image
 
+
 def test_invalid_file():
     bad_file = os.path.join(os.path.dirname(__file__), "not_a_real_file.jp2")
     with pytest.raises(FileNotFoundError):
         my_image = Image(bad_file)
+
 
 def test_image2_filename():
     sample_file = os.path.join(os.path.dirname(__file__), "sample_images/dummy.jp2")
@@ -26,8 +28,8 @@ def test_image2_pixelWidth():
     assert isinstance(my_image.pixelWidth, int)
     assert my_image.pixelWidth == 4000
 
-def test_image2_exif():
 
+def test_image2_exif():
     sample_file = os.path.join(os.path.dirname(__file__), "sample_images/dummy.jp2")
     my_image = Image(sample_file)
     exivf_metadata = my_image.exif
@@ -35,15 +37,12 @@ def test_image2_exif():
     assert exivf_metadata['Exif.Image.Artist'] == "University of Illinois Library"
 
 
-
 def test_image2_iptc():
-
     sample_file = os.path.join(os.path.dirname(__file__), "sample_images/dummy.jp2")
     my_image = Image(sample_file)
     iptc_metadata = my_image.iptc
     assert isinstance(iptc_metadata, dict)
     assert iptc_metadata['Iptc.Application2.ObjectName'] == "Mapping History - University Archives"
-
 
 
 def test_image_get_icc_data():
