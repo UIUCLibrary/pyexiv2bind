68500eb1a051875a35485cc06d81973b0054cd28
Henry Borchers <henryborchers@yahoo.com>
2018-10-10 14:24:33 -0500

    test tiffs

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,11 +8,25 @@ def test_exiv_version():
     assert isinstance(exiv2_version, str)
 
 
-def test_set_dpi(sample_images_editable):
+def test_set_dpi_on_jp2(sample_images_editable):
     test_jp2 = os.path.join(sample_images_editable, "dummy.jp2")
     sample_image = core.Image(test_jp2)
     assert sample_image.exif['Exif.Image.XResolution'] == "400/1"
     assert sample_image.exif['Exif.Image.YResolution'] == "400/1"
+    assert sample_image.exif['Exif.Image.ResolutionUnit'] == "2"
+
+    # core.set_dpi(test_jp2, 300, 300)
+    core.set_dpi(image=str(test_jp2), x=300, y=300)
+    sample_image2 = core.Image(test_jp2)
+    assert sample_image2.exif['Exif.Image.XResolution'] == "300/1"
+    assert sample_image2.exif['Exif.Image.YResolution'] == "300/1"
+    assert sample_image2.exif['Exif.Image.ResolutionUnit'] == "2"
+
+def test_set_dpi_on_tiff(sample_images_editable):
+    test_jp2 = os.path.join(sample_images_editable, "dummy.tif")
+    sample_image = core.Image(test_jp2)
+    assert sample_image.exif['Exif.Image.XResolution'] != "400/1"
+    assert sample_image.exif['Exif.Image.YResolution'] != "400/1"
     assert sample_image.exif['Exif.Image.ResolutionUnit'] == "2"
 
     # core.set_dpi(test_jp2, 300, 300)
