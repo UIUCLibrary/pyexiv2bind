13c3b01e195010abddaa5397dd7c2613aa605fd4
henry <henryborchers@yahoo.com>
2020-09-21 20:56:12 -0500

    Add conanfile.py for managing dependencies

--- cmake/external_exiv22.cmake
+++ cmake/external_exiv22.cmake
@@ -42,53 +42,6 @@ function(patch_tiff_resolution root)
     execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different "${libexiv2_BINARY_DIR}/tiffimage_int.cpp" "${tiffimage_int}")
 endfunction()
 
-###########################################################################################
-# Google ZLIB
-###########################################################################################
-
-FetchContent_Declare(zlib
-        URL https://zlib.net/zlib-1.2.11.tar.gz
-        URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
-        PATCH_COMMAND ${CMAKE_COMMAND} -P ${PROJECT_BINARY_DIR}/patch_zlib.cmake
-        )
-FetchContent_GetProperties(zlib)
-configure_file(${PROJECT_SOURCE_DIR}/patches/patch_zlib.cmake patch_zlib.cmake @ONLY)
-
-if (NOT zlib_POPULATED)
-    FetchContent_Populate(zlib)
-    set(SKIP_INSTALL_LIBRARIES YES CACHE BOOL "")
-    set(BUILD_examples NO CACHE BOOL "")
-#
-    add_subdirectory(${zlib_SOURCE_DIR} ${zlib_BINARY_DIR} EXCLUDE_FROM_ALL)
-    get_directory_property(zlib_targets DIRECTORY ${zlib_SOURCE_DIR} BUILDSYSTEM_TARGETS )
-    foreach(target ${zlib_targets})
-        if(${target} STREQUAL example)
-            message(FATAL_ERROR "ZLib imported the \"${target}\" Target.")
-        endif()
-    endforeach()
-#    set_tests_properties(example PROPERTIES DISABLED ON)
-endif ()
-
-
-###########################################################################################
-# libexpat build
-###########################################################################################
-
-FetchContent_Declare(expat
-        URL https://github.com/libexpat/libexpat/archive/R_2_2_4.zip
-        )
-
-FetchContent_GetProperties(expat)
-if (NOT expat_POPULATED)
-    FetchContent_Populate(expat)
-    option(BUILD_examples "" OFF)
-    option(BUILD_shared "" OFF)
-    option(BUILD_tests "" NO)
-    option(BUILD_tools "" NO)
-#    option(INSTALL "" NO)
-
-    add_subdirectory(${expat_SOURCE_DIR}/expat ${expat_BINARY_DIR})
-endif ()
 FetchContent_Declare(
         libexiv2
         URL https://github.com/Exiv2/exiv2/archive/0.27.tar.gz
@@ -97,46 +50,19 @@ FetchContent_Declare(
 #        PATCH_COMMAND
 #            COMMAND git apply ${PROJECT_SOURCE_DIR}/patches/tiff_resolution_path.patch
 )
-#if(EXIV2_VERSION_TAG)
-#
-#else()
-#    message(STATUS "Using version 0.27")
-#    FetchContent_Declare(
-#            libexiv2
-#            GIT_REPOSITORY https://github.com/Exiv2/exiv2.git
-#            GIT_TAG "0.27"
-#            PATCH_COMMAND
-#                COMMAND git apply ${PROJECT_SOURCE_DIR}/patches/tiff_resolution_path.patch
-#    )
-#endif()
 
 FetchContent_GetProperties(libexiv2)
 if (NOT libexiv2_POPULATED)
     FetchContent_Populate(libexiv2)
     patch_tiff_resolution("${libexiv2_SOURCE_DIR}")
     patch_exiv2_cmake("${libexiv2_SOURCE_DIR}")
-
-    option(BUILD_SHARED_LIBS "" OFF)
-    set(BUILD_SHARED_LIBS OFF CACHE BOOL "")
     set(EXIV2_BUILD_SAMPLES OFF CACHE BOOL "")
-    option(EXIV2_BUILD_UNIT_TESTS "" OFF)
-
+    option(EXIV2_BUILD_SAMPLES "" OFF)
     if(MSVC)
         option(EXIV2_ENABLE_DYNAMIC_RUNTIME "" ON)
     endif()
 
-    set(EXPAT_LIBRARY $<TARGET_FILE:expat>)
-    set(EXPAT_INCLUDE_DIR $<TARGET_PROPERTY:expat,INCLUDE_DIRECTORIES>)
-    set(ZLIB_INCLUDE_DIR $<TARGET_PROPERTY:zlibstatic,INCLUDE_DIRECTORIES>)
-    set(ZLIB_LIBRARY $<TARGET_FILE:zlibstatic>)
-    set(EXIV2_BUILD_SAMPLES OFF)
-    option(EXIV2_BUILD_SAMPLES "" OFF)
     include_directories(${libexiv2_BINARY_DIR})
     add_subdirectory(${libexiv2_SOURCE_DIR} ${libexiv2_BINARY_DIR})
-#    target_include_directories(exiv2lib PRIVATE ${libexiv2_BINARY_DIR})
-#    include_directories(${libexiv2_BINARY_DIR})
-    add_dependencies(exiv2lib zlibstatic)
 endif ()
-add_dependencies(exiv2lib expat)
-#install(DIRECTORY ${libexiv2_SOURCE_DIR}/include/ DESTINATION include)
 
