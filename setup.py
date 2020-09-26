d8ac411a8b4c341e3ed3d6ef7e436bcbfb57fb8e
henry <henryborchers@yahoo.com>
2020-09-26 13:26:01 -0500

    resolve for mac and windows

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+import abc
 import json
 import os
 import re
 import sys
 import shutil
 import tarfile
+import tempfile
 from typing import List, Optional, Tuple, Iterable, Dict, Any, Union
 from urllib import request
 
@@ -588,6 +590,242 @@ def remove_system_dlls(dlls):
     return non_system_dlls
 
 
+class AbsSoHandler(abc.ABC):
+    def __init__(self, library_file, context):
+        self.library_file = library_file
+        self._compiler = None
+        self.context = context
+
+    def set_compiler(self, compiler):
+        self._compiler = compiler
+
+    @classmethod
+    def is_system_file(cls, filename) -> bool:
+        return False
+
+    @abc.abstractmethod
+    def get_deps(self) -> List[str]:
+        pass
+
+    def resolve(self, search_paths=None) -> None:
+        dest = os.path.dirname(self.library_file)
+        for dep in filter(lambda x: not self.is_system_file(x),
+                          self.get_deps()):
+
+            if os.path.exists(os.path.join(dest, dep)):
+                continue
+            if search_paths is None:
+                if platform.system() == "Windows":
+                    search_paths = self.context.compiler.library_dirs + \
+                                   os.environ['path'].split(";")
+                else:
+                    search_paths = self.context.compiler.library_dirs + \
+                                   os.environ['PATH'].split(":")
+
+            dll = self.context.find_deps(dep, search_paths)
+            if dll is None:
+                raise FileNotFoundError(f"Unable to locate {dep} for "
+                                        f"{self.library_file}")
+            if not self.is_system_file(dll):
+                type(self)(dll, self.context).resolve(search_paths)
+
+class NullHandlerStrategy(AbsSoHandler):
+
+    def get_deps(self) -> List[str]:
+        return []
+
+
+class MacholibStrategy(AbsSoHandler):
+    _system_files = []
+
+    @classmethod
+    def get_system_files(cls):
+        if len(cls._system_files) == 0:
+            cls._system_files = os.listdir("/usr/lib")
+        return cls._system_files
+
+    def get_deps(self) -> List[str]:
+        from macholib import MachO
+        libs = set()
+        for header in MachO.MachO(self.library_file).headers:
+            for _idx, _name, other in header.walkRelocatables():
+                libs.add(os.path.split(other)[-1])
+        return list(libs)
+
+    @classmethod
+    def is_system_file(cls, filename) -> bool:
+        if filename in [
+            "libsystem_malloc.dylib"
+        ]:
+            return True
+
+        if filename in cls.get_system_files():
+            return True
+        return False
+
+    def resolve(self, search_path=None) -> None:
+        from macholib import MachOStandalone
+        if self.is_system_file(os.path.split(self.library_file)[1]):
+            return
+
+        d = MachOStandalone.MachOStandalone(
+            os.path.abspath(os.path.dirname(self.library_file)))
+
+        d.dest = os.path.abspath(os.path.dirname(self.library_file))
+        libraries = []
+        for dep in self.get_deps():
+            if self.is_system_file(dep):
+                continue
+            found_dep = self.context.find_deps(dep, search_path)
+            c_dep = d.copy_dylib(found_dep)
+            dep_file = d.mm.locate(c_dep)
+            if dep_file is None:
+                raise FileNotFoundError(f"Unable to locate {dep}, "
+                                        f"required by {self.library_file}")
+
+            self.context.announce(f"Fixing up {dep}")
+
+            libraries.append(dep_file)
+            type(self)(dep_file, self.context).resolve(search_path)
+        libraries.append(self.library_file)
+        d.run(platfiles=libraries, contents="@rpath/..")
+
+
+
+class AudidWheelsHandlerStrategy(AbsSoHandler):
+
+    def get_deps(self) -> List[str]:
+        return []
+
+def get_so_handler(shared_library: str, context,
+                   system_name: str = None) -> AbsSoHandler:
+
+    system_name = system_name or platform.system()
+    strategies = {
+        # "Darwin": NullHandlerStrategy,
+        "Darwin": MacholibStrategy,
+        "Linux": AudidWheelsHandlerStrategy,
+        "Windows": DllHandlerStrategy,
+    }
+    strat = strategies.get(
+        system_name, NullHandlerStrategy)
+    return strat(shared_library, context)
+
+
+class AbsSoHandler(abc.ABC):
+    def __init__(self, library_file, context):
+        self.library_file = library_file
+        self._compiler = None
+        self.context = context
+
+    def set_compiler(self, compiler):
+        self._compiler = compiler
+
+    @classmethod
+    def is_system_file(cls, filename) -> bool:
+        return False
+
+    @abc.abstractmethod
+    def get_deps(self) -> List[str]:
+        pass
+
+    def resolve(self, search_paths=None) -> None:
+        dest = os.path.dirname(self.library_file)
+        for dep in filter(lambda x: not self.is_system_file(x),
+                          self.get_deps()):
+
+            if os.path.exists(os.path.join(dest, dep)):
+                continue
+            if search_paths is None:
+                if platform.system() == "Windows":
+                    search_paths = self.context.compiler.library_dirs + \
+                                   os.environ['path'].split(";")
+                else:
+                    search_paths = self.context.compiler.library_dirs + \
+                                   os.environ['PATH'].split(":")
+
+            dll = self.context.find_deps(dep, search_paths)
+            if dll is None:
+                raise FileNotFoundError(f"Unable to locate {dep} for "
+                                        f"{self.library_file}")
+            if not self.is_system_file(dll):
+                type(self)(dll, self.context).resolve(search_paths)
+
+
+class DllHandlerStrategy(AbsSoHandler):
+    def __init__(self, library_file, context):
+        super().__init__(library_file, context)
+        self._compiler = None
+
+    def resolve(self, search_paths=None) -> None:
+        dest = os.path.dirname(self.library_file)
+        for dep_name in self.get_deps():
+            if self.is_system_file(dep_name):
+                continue
+            dep = self.find_lib(dep_name, search_paths=search_paths)
+            if not dep:
+                raise FileNotFoundError(
+                    f"Unable to locate {dep_name} required by "
+                    f"{self.library_file}")
+
+            new_dll = os.path.join(dest, os.path.split(dep)[-1])
+            if os.path.exists(new_dll):
+                continue
+            self.context.copy_file(dep, new_dll)
+            DllHandlerStrategy(new_dll, self.context).resolve(search_paths)
+
+    def find_lib(self, lib, search_paths=None):
+        if search_paths is None:
+            search_paths = os.environ['path'].split(";")
+
+        for path in search_paths:
+            if not os.path.exists(path):
+                self.context.announce(f"Skipping invalid path: {path}", 5)
+                continue
+            for f in os.scandir(path):
+                if f.name.lower() == lib.lower():
+                    return f.path
+
+    @classmethod
+    def is_system_file(cls, filename: str) -> bool:
+        system_libs = [
+            i for i in os.listdir(r"c:\Windows\System32") if i.endswith(".dll")
+        ]
+
+        if filename in system_libs:
+            return True
+
+        if "api-ms-win-crt" in filename:
+            return True
+
+        if filename.startswith("python"):
+            return True
+        if filename == "KERNEL32.dll":
+            return True
+        return False
+
+    def get_deps(self) -> List[str]:
+        if not self.context.compiler.initialized:
+            self.context.compiler.initialize()
+        so_name = os.path.split(self.library_file)[-1]
+        with tempfile.TemporaryDirectory() as td:
+            output_file = os.path.join(td, f'{so_name}.dependents')
+            dumpbin = \
+                shutil.which("dumpbin",
+                             path=os.path.dirname(self.context.compiler.cc))
+
+            self.context.compiler.spawn(
+                [
+                    dumpbin,
+                    '/dependents',
+                    os.path.abspath(self.library_file),
+                    f'/out:{output_file}'
+                ]
+            )
+            return BuildPybind11Extension.parse_dumpbin_deps(file=output_file)
+
+
+
 class BuildPybind11Extension(build_ext):
     user_options = build_ext.user_options + [
         ('pybind11-url=', None,
@@ -620,27 +858,68 @@ class BuildPybind11Extension(build_ext):
         for e in self.extensions:
             dll_name = \
                 os.path.join(self.build_lib, self.get_ext_filename(e.name))
+            search_dirs = self.get_library_paths()
+            self.resolve_shared_library(dll_name, search_dirs)
+            # dll_name = \
+            #     os.path.join(self.build_lib, self.get_ext_filename(e.name))
 
-            output_file = os.path.join(self.build_temp, f'{e.name}.dependents')
-            if self.compiler.compiler_type != "unix":
-                if not self.compiler.initialized:
-                    self.compiler.initialize()
-                self.compiler.spawn(
-                    [
-                        'dumpbin',
-                        '/dependents',
-                        dll_name,
-                        f'/out:{output_file}'
-                    ]
+            #
+            # output_file = os.path.join(self.build_temp, f'{e.name}.dependents')
+            # if self.compiler.compiler_type != "unix":
+            #     if not self.compiler.initialized:
+            #         self.compiler.initialize()
+            #     self.compiler.spawn(
+            #         [
+            #             'dumpbin',
+            #             '/dependents',
+            #             dll_name,
+            #             f'/out:{output_file}'
+            #         ]
+            #     )
+            #     deps = parse_dumpbin_deps(file=output_file)
+            #     deps = remove_system_dlls(deps)
+            #     dest = os.path.dirname(dll_name)
+            #     for dep in deps:
+            #
+            #         dll = self.find_deps(dep)
+            #         if dll is None:
+            #             raise FileNotFoundError(f"Unable to locate required library {dep}")
+            #         shutil.copy(dll, dest)
+    def get_library_paths(self):
+        search_paths = []
+        library_search_paths = \
+            self.compiler.library_dirs + \
+            self.compiler.runtime_library_dirs + \
+            self.library_dirs + \
+            self._get_path_dirs()
+
+        for lib_path in library_search_paths:
+            if not os.path.exists(lib_path):
+                continue
+            if lib_path in search_paths:
+                continue
+            search_paths.append(lib_path)
+
+        return search_paths
+    def _get_path_dirs(self):
+        if platform.system() == "Windows":
+            paths = os.environ['path'].split(";")
+        else:
+            paths = os.environ['PATH'].split(":")
+        return [path for path in paths if os.path.exists(path)]
+
+    def resolve_shared_library(self, dll_name, search_paths=None):
+        dll_dumper = get_so_handler(dll_name, self)
+        dll_dumper.set_compiler(self.compiler)
+        try:
+            dll_dumper.resolve(search_paths)
+        except FileNotFoundError:
+            if search_paths is not None:
+                self.announce(
+                    "Error: Not all required shared libraries were resolved. "
+                    "Searched:\n{}".format('\n'.join(search_paths)), 5
                 )
-                deps = parse_dumpbin_deps(file=output_file)
-                deps = remove_system_dlls(deps)
-                dest = os.path.dirname(dll_name)
-                for dep in deps:
-                    dll = self.find_deps(dep)
-                    if dll is None:
-                        raise FileNotFoundError(f"Unable to locate required library {dep}")
-                    shutil.copy(dll, dest)
+            raise
 
     def find_deps(self, lib):
         # TODO replace with one that search directores
