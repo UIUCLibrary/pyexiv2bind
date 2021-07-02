38e55fe87a4d0699cc75818543c19dca5422c198
henry <henryborchers@yahoo.com>
2021-07-02 10:20:31 -0500

    bind is_good

--- py3exiv2bind/core/core.cpp
+++ py3exiv2bind/core/core.cpp
@@ -32,8 +32,9 @@ PYBIND11_MODULE(core, m) {
             .def_property_readonly("warnings_logs", &Image::getWarning_logs,   "Warnings produced by Exiv2 library")
             .def("get_icc_profile_data",            [](const Image &i) {
                      return pybind11::bytes(i.get_icc_profile());
-                 },                                                            "Get the icc profile data"
-            );
+                 },                                                            "Get the icc profile data")
+            .def("is_good",                  &Image::is_good,     "Check if a file is loaded.")
+            ;
     pybind11::register_exception<NoIccError>(m, "NoICCError");
 
     m.def("set_dpi", &set_dpi,
