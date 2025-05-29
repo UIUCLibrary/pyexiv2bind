098980325fd8a717e0e89a2edf8eb026b8d788f3
Henry Borchers <henryborchers@yahoo.com>
2025-05-29 13:03:22 -0500

    tox environments uses version contrains

--- contrib/get_linked_exiv2_version.py
+++ contrib/get_linked_exiv2_version.py
@@ -1,6 +1,10 @@
 def main():
-    from py3exiv2bind import core
-    print(f'Testing against exiv2 version {core.exiv2_version()}')
+    try:
+        from py3exiv2bind import core
+        print(f'Testing against exiv2 version {core.exiv2_version()}')
+    except ImportError:
+        print("py3exiv2bind.core is not built")
+        return
 
 if __name__ == '__main__':
     main()
