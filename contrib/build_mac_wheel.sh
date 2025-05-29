ec4d4f15080e8bddef1ec7001df9470f45830720
Henry Borchers <henryborchers@yahoo.com>
2025-05-29 15:20:18 -0500

    ci: simplify build mac wheel script

--- contrib/build_mac_wheel.sh
+++ contrib/build_mac_wheel.sh
@@ -12,16 +12,6 @@ remove_venv(){
     fi
 }
 
-# generate_venv(){
-#     base_python=$1
-#     virtual_env=$2
-#     trap "remove_venv $virtual_env" ERR SIGINT SIGTERM
-#     $base_python -m venv $virtual_env
-#     . $virtual_env/bin/activate
-#     pip install uv
-#     UV_INDEX_STRATEGY=unsafe-best-match uv pip install wheel==0.37 build delocate
-# }
-
 generate_venv_with_venv(){
     base_python=$1
     virtual_env=$2
@@ -29,55 +19,6 @@ generate_venv_with_venv(){
     $base_python -m venv $virtual_env
     $virtual_env/bin/pip install --disable-pip-version-check uv
 }
-
-#
-# generate_wheel(){
-#     virtual_env=$1
-#     . $virtual_env/bin/activate
-#     python --version
-#
-#     # Get the processor type
-#     processor_type=$(uname -m)
-#
-#     # Set the compiling variables based on the processor results
-#     #
-#     # The values are taken from cibuildwheel source code
-#     # https://github.com/pypa/cibuildwheel/blob/main/cibuildwheel/macos.py
-#     #
-#     # macOS 11 is the first OS with arm64 support, so the wheels
-#     # have that as a minimum.
-#
-#     if [ "$processor_type" == "arm64" ]; then
-#         _PYTHON_HOST_PLATFORM='macosx-11.0-arm64'
-#         MACOSX_DEPLOYMENT_TARGET='11.0'
-#         ARCHFLAGS='-arch arm64'
-#         REQUIRED_ARCH='arm64'
-#     elif [ "$processor_type" == "x86_64" ]; then
-#         _PYTHON_HOST_PLATFORM='macosx-10.15-x86_64'
-#         MACOSX_DEPLOYMENT_TARGET='10.15'
-#         ARCHFLAGS='-arch x86_64'
-#         REQUIRED_ARCH='x86_64'
-#     else
-#       echo "Unknown processor type: $processor_type"
-#     fi
-#
-#     out_temp_wheels_dir=$(mktemp -d /tmp/python_wheels.XXXXXX)
-#     output_path="./dist"
-#     trap "rm -rf $out_temp_wheels_dir" ERR SIGINT SIGTERM RETURN
-#     UV_INDEX_STRATEGY=unsafe-best-match _PYTHON_HOST_PLATFORM=$_PYTHON_HOST_PLATFORM MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET ARCHFLAGS=$ARCHFLAGS python -m build --wheel --installer=uv --outdir=$out_temp_wheels_dir
-#     pattern="$out_temp_wheels_dir/*.whl"
-#     files=( $pattern )
-#     undelocate_wheel="${files[0]}"
-#
-#     echo ""
-#     echo "================================================================================"
-#     echo "${undelocate_wheel} is linked to the following:"
-#     delocate-listdeps --depending "${undelocate_wheel}"
-#     echo ""
-#     echo "================================================================================"
-#     delocate-wheel -w $output_path --require-archs $REQUIRED_ARCH --verbose "$undelocate_wheel"
-# }
-
 
 generate_wheel(){
     uv_exec=$1
@@ -200,49 +141,9 @@ while [[ "$#" -gt 0 ]]; do
   esac
 done
 
-# # venv_path value is set to default
-# venv_path=$DEFAULT_PYTHON_VENV
-#
-# # base_python_path value is set to default
-# base_python_path=$DEFAULT_BASE_PYTHON
-#
-# # Parse optional arguments
-# while [[ "$#" -gt 0 ]]; do
-#   case $1 in
-#     --venv-path=*)
-#       venv_path="${1#*=}"
-#       shift
-#       ;;
-#     --venv-path)
-#       venv_path="$2"
-#       shift 2
-#       ;;
-#
-#     --base-python=*)
-#       base_python_path="${1#*=}"
-#       shift
-#       ;;
-#     --base-python)
-#       base_python_path="$2"
-#       shift 2
-#       ;;
-#
-#     *)
-#       shift
-#       ;;
-#   esac
-# done
-
 # Output the values
 
 check_args
-
-# build_virtual_env=$venv_path
-# if [[ ! -f "$build_virtual_env/bin/python" ]]; then
-#     generate_venv $base_python_path $build_virtual_env
-# else
-#     echo "Using existing venv: $build_virtual_env"
-# fi
 
 uv_path=uv
 if [[ ! -f "$uv_path" ]]; then
@@ -254,5 +155,5 @@ if [[ ! -f "$uv_path" ]]; then
 else
     echo "Using existing venv: $build_virtual_env"
 fi
-# generate_wheel $build_virtual_env $project_root
+
 generate_wheel $uv_path $project_root $python_version
